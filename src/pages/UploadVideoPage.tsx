6d8bf9fd9f6a884efa4f80c92daeba4b4832ba51
uribeIsaac26 <uribearciniegasisaac@gmail.com>
2026-03-07 16:14:12 -0500

    se sube la cantidad de videos permitida

--- src/pages/UploadVideoPage.tsx
+++ src/pages/UploadVideoPage.tsx
@@ -68,7 +68,7 @@ function UploadVideoPage() {
 
     setIsUploading(true);
 
-    const MAX_CONCURRENT = 10;
+    const MAX_CONCURRENT = 20;
 
     for (let i = 0; i < videos.length; i += MAX_CONCURRENT) {
       const chunk = videos.slice(i, i + MAX_CONCURRENT);
